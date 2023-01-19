3d26c668d77fd6bd4c862123df8f5f710165a052
Ruslan <heeca@mail.ru>
2023-01-19 13:53:44 +0300

    Fix GetUsers

--- src/guard/basicAuthGuard.ts
+++ src/guard/basicAuthGuard.ts
@@ -6,7 +6,11 @@ export class BasicAuthGuard implements CanActivate {
     canActivate(context: ExecutionContext): boolean | Promise<boolean> | Observable<boolean> {
         const request = context.switchToHttp().getRequest();
         const header = request.header('Authorization');
+        if (!header) {
+            throw new UnauthorizedException();
+        }
         const matchArr = header.match(/^Basic (.*)$/);
+        if (!matchArr) throw new UnauthorizedException();
         const [login, passwd] = Buffer.from(matchArr[1], 'base64').toString().split(':');
         if (login === 'admin' && passwd === 'qwerty') {
             return true;
