d2612691b2f1fce5d8210c567e21f52a0e0593d5
Ruslan <heeca@mail.ru>
2023-01-24 06:23:39 +0300

    fix email.service.ts

--- src/helper/email.service.ts
+++ src/helper/email.service.ts
@@ -5,15 +5,19 @@ import { MailerService } from '@nestjs-modules/mailer';
 export class EmailService {
     constructor(private readonly mailerService: MailerService) {}
     async sendEmail(email: string, subject: string, confirmationCode: string) {
-        this.mailerService.sendMail({
-            from: '"RUSEL" <heeca@mail.ru>', // sender address
-            to: email,
-            subject: subject,
-            html: `<h1>Thank for your registration</h1>
+        try {
+            await this.mailerService.sendMail({
+                from: '"RUSEL" <heeca@mail.ru>', // sender address
+                to: email,
+                subject: subject,
+                html: `<h1>Thank for your registration</h1>
                         <p>To finish registration please follow the link below:
                              <a href='https://somesite.com/confirm-email?code=${confirmationCode}'>complete registration</a>
                         </p>`,
-        });
+            });
+        } catch (e) {
+            return true;
+        }
     }
 
     async sendMailRecoveryPassword(email: string, subject: string, NewRecoveryCode: string) {
--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -54,6 +54,7 @@ export class UsersService {
             },
         };
         const result = await this.usersRepository.createUser({ ...newUser });
+        this.emailService.sendEmail(newUser.accountData.email, 'Registr', newUser.emailConfirmation.confirmationCode);
         if (!result)
             throw new BadRequestException({
                 message: 'Bad',
