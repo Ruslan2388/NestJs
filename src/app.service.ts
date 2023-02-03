cb7f654c1a973f46d456fd561e3fdafc298b0024
Ruslan <heeca@mail.ru>
2023-02-03 20:53:54 +0300

    End14eEB

--- src/app.service.ts
+++ src/app.service.ts
@@ -3,6 +3,6 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
     getHello(): string {
-        return 'Аxуенно Все РАБОТАЕТ !!!!';
+        return 'Это блять работает';
     }
 }
