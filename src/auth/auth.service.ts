ea4d999123947eb51837a2ae4641e03260d0f56f
Ruslan <heeca@mail.ru>
2023-03-05 23:53:02 +0300

    fix 500

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -18,11 +18,11 @@ export class AuthService {
     ) {}
 
     async createAccessToken(userId: string, deviceId: string) {
-        return this.jwtService.sign({ userId: userId, deviceId: deviceId }, { secret: 'SecretKey', expiresIn: '40m' });
+        return this.jwtService.sign({ userId: userId, deviceId: deviceId }, { secret: 'SecretKey', expiresIn: '10s' });
     }
 
     async createRefreshToken(userId: string, deviceId: string) {
-        return this.jwtService.sign({ userId: userId, deviceId: deviceId }, { secret: 'SecretKey', expiresIn: '60m' });
+        return this.jwtService.sign({ userId: userId, deviceId: deviceId }, { secret: 'SecretKey', expiresIn: '20s' });
     }
 
     async getIatAndExpToken(refreshToken: string) {
