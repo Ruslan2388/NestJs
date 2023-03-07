3299891dc4eaf84e85efedd56908de52bd75141c
Ruslan <heeca@mail.ru>
2023-03-07 19:12:41 +0300

    fix getcommmentsBypostId

--- src/app.service.ts
+++ src/app.service.ts
@@ -3,6 +3,6 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
     getHello(): string {
-        return 'Это блять работает';
+        return 'Hello world';
     }
 }
