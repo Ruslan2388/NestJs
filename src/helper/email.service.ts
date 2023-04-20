cbbd7d74b7fd622dc97228f72f5047281ec58999
Ruslan <heeca@mail.ru>
2023-04-20 03:20:18 +0300

    fix getcommmentsBypostId

--- src/helper/email.service.ts
+++ src/helper/email.service.ts
@@ -6,7 +6,7 @@ export class EmailService {
     constructor(private readonly mailerService: MailerService) {}
     async sendEmail(email: string, subject: string, confirmationCode: string) {
         try {
-            this.mailerService.sendMail({
+            await this.mailerService.sendMail({
                 from: '"RUSEL" <it-project-inctagram2000@mail.ru>', // sender address
                 to: email,
                 subject: subject,
@@ -24,7 +24,7 @@ export class EmailService {
     }
 
     async sendMailRecoveryPassword(email: string, subject: string, NewRecoveryCode: string) {
-        this.mailerService.sendMail({
+        await this.mailerService.sendMail({
             from: '"RUSEL" <it-project-inctagram2000@mail.ru>', // sender address
             to: email,
             subject: subject,
